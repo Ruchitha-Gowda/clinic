26e3d8b04ee036c3ad8c935c474c9f84387bd4ea
Ruchitha-Gowda <ruchithabr9@gmail.com>
2025-08-11 01:48:30 +0530

    Updated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,28 +1,30 @@
-import {Module, Injectable} from "@nestjs/common";
+import { Module, Injectable } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import {AuthModule} from "./auth/auth.module";
+import { AuthModule } from "./auth/auth.module";
 import { DoctorModule } from './doctors/doctor.module';
 import { PatientModule } from './patients/patient.module';
 import { AppointmentModule } from './appointments/appointment.module';
 import { QueueModule } from './queue/queue.module';
-import {ConfigModule, ConfigService} from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
-@Module ({
+@Module({
   imports: [
-    ConfigModule.forRoot ({
+    ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env"
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: parseInt(process.env.DB_PORT ?? '3306', 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
+
     AuthModule,
     DoctorModule,
     PatientModule,
@@ -31,5 +33,5 @@ import {ConfigModule, ConfigService} from "@nestjs/config";
   ]
 })
 
-export class AppModule{}
+export class AppModule { }
 
